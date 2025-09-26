a883be06558c48757c9feb0b57226c960a361cc6
SHson <2021111705@dgu.ac.kr>
2025-09-26 15:11:40 +0900

    Update submodule content

--- subject5/HW.py
+++ subject5/HW.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요: ").strip()
 id  = input("학번을 입력하세요: ").strip()
+major  = input("학과를 입력하세요: ").strip()
 
 print("\n<출력>")
 print(f"이름: {name}")
 print(f"학번: {id}")
+print(f"학과: {major}")
